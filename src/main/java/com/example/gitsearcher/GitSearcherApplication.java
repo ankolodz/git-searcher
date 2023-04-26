e6b44df947eb7e33efe5723d8a1b4ba3b573450e
Andrzej <ankolodz@gmail.com>
2023-04-26 18:00:33 +0200

    Initial commit

---
+++ src/main/java/com/example/gitsearcher/GitSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSearcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitsearcher/GitSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSearcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
