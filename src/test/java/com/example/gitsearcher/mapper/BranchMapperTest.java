ae4724cb0066397541d1365409ff3997d0154618
Andrzej <ankolodz@gmail.com>
2023-04-27 22:36:18 +0200

    Add unit test

---
+++ src/test/java/com/example/gitsearcher/mapper/BranchMapperTest.java
@@ -0,0 +1,26 @@
+package com.example.gitsearcher.mapper;
+
+import com.example.gitsearcher.dto.GitBranchDto;
+import com.example.gitsearcher.model.GitHubBranchResponse;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+
+import static com.example.gitsearcher.service.StaticResponse.getExample;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BranchMapperTest {
+
+    @Test
+    void shouldCorrectMapBranch(){
+        BranchMapper branchMapper = new BranchMapper();
+        GitHubBranchResponse response = getExample()[0];
+
+        GitBranchDto result = branchMapper.toDto(response);
+
+        GitBranchDto correctResult = new GitBranchDto("master", "c5b97d5ae6c19d5c5df71a34c7fbeeda2479ccbc");
+
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(correctResult);
+    }
+}
--- src/test/java/com/example/gitsearcher/service/StaticResponse.java
+++ src/test/java/com/example/gitsearcher/service/StaticResponse.java
@@ -1,8 +1,16 @@
 package com.example.gitsearcher.service;
 
+import com.example.gitsearcher.model.GitHubBranchResponse;
+import com.google.gson.Gson;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class StaticResponse {
     public static String EXAMPLE_BRANCH_RESPONSE = "[ { \"name\": \"master\", \"commit\": { \"sha\": \"c5b97d5ae6c19d5c5df71a34c7fbeeda2479ccbc\", \"url\": \"https://api.github.com/repos/octocat/Hello-World/commits/c5b97d5ae6c19d5c5df71a34c7fbeeda2479ccbc\" }, \"protected\": true, \"protection\": { \"required_status_checks\": { \"enforcement_level\": \"non_admins\", \"contexts\": [ \"ci-test\", \"linter\" ] } }, \"protection_url\": \"https://api.github.com/repos/octocat/hello-world/branches/master/protection\" } ]";
+
+    public static GitHubBranchResponse[] getExample(){
+        final Gson gson = new Gson();
+
+        return gson.fromJson(EXAMPLE_BRANCH_RESPONSE, GitHubBranchResponse[].class);
+    }
 }
