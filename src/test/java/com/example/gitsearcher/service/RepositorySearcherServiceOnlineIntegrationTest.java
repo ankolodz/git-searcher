d875ff7fe092fa117b02058ac880b31d2048aeed
Andrzej <ankolodz@gmail.com>
2023-05-06 15:55:27 +0200

    Add MockService into tests

--- src/test/java/com/example/gitsearcher/service/RepositorySearcherServiceOnlineIntegrationTest.java
+++ src/test/java/com/example/gitsearcher/service/RepositorySearcherServiceOnlineIntegrationTest.java
@@ -1,30 +1,80 @@
 package com.example.gitsearcher.service;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockserver.integration.ClientAndServer;
+import org.mozilla.javascript.tools.shell.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.AfterTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.stop.Stop.stopQuietly;
 
 @SpringBootTest
 class RepositorySearcherServiceOnlineIntegrationTest {
 
+    private static ClientAndServer proxy = ClientAndServer.startClientAndServer();
+    ;
+//    @Resource
+//    private Environment environment;
+//    @Resource
+//    private WebApplicationContext webApplicationContext;
+
     @Autowired
     private RepositorySearcherService repositorySearcherService;
 
+    @BeforeClass
+    public static void startProxy() {
+        System.setProperty("https://api.github.com", "127.0.0.1");
+    }
+
+    @AfterClass
+    public static void stopProxy() {
+        stopQuietly(proxy);
+        System.clearProperty("https://api.github.com");
+    }
+
+    @BeforeTest
+    public void startMockServer() {
+        proxy.reset();
+    }
+
     @Test
-    @Disabled("The test uses a large part of github's quota")
     void shouldReturnAllRepository() throws IOException, InterruptedException {
-        assertEquals(11, repositorySearcherService.getRepositories("ankolodz").size());
+        proxy.when(request()
+                        .withPath("users/ankolodz/repos"))
+                .respond(
+                        response().withBody(StaticResponse.EXAMPLE_REPOSITORY_RESPONSE)
+                );
+
+        proxy.when(request().withPath("/branches"))
+                .respond(response().withBody(StaticResponse.EXAMPLE_BRANCH_RESPONSE));
+
+        assertEquals(9, repositorySearcherService.getRepositories("ankolodz").size());
     }
 
     @Test
     void shouldThrowErrorWhenUserNotExist() {
         String exceptionType = "";
         try {
+            proxy.when(request()
+                            .withPath("users/111notExistingUsers222/repos"))
+                    .respond(
+                            response().withStatusCode(404)
+                    );
+
             repositorySearcherService.getRepositories("111notExistingUsers222");
         } catch (Exception e) {
             exceptionType = e.getClass().getName();
